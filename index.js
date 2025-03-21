22bc58470168e2aaec57c22d3194f2c434779775
Facu <65638351+facusis@users.noreply.github.com>
2025-03-21 20:08:53 +0100

    funcionalidad de js para validacion y dinamismo entre paginas terminada.

---
+++ index.js
@@ -0,0 +1,16 @@
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   const menuToggle = document.querySelector(".menu-toggle"); // Botón hamburguesa
+//   const navMenu = document.querySelector(".nav-menu"); // Menú
+
+//   menuToggle.addEventListener("click", () => {
+//       navMenu.classList.toggle("show-menu"); // Alterna la clase para mostrar/ocultar
+//   });
+
+//   // Cerrar menú si se hace clic fuera
+//   document.addEventListener("click", (event) => {
+//       if (!menuToggle.contains(event.target) && !navMenu.contains(event.target)) {
+//           navMenu.classList.remove("show-menu");
+//       }
+//   });
+// });
